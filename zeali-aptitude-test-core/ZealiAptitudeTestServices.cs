00d514d13c49c6bf1fa3d0924bb2a6c48c85e808
KarthickBoopathy <Karthickboopathy94@gmail.com>
2021-09-03 16:42:25 +0530

    test

--- zeali-aptitude-test-core/ZealiAptitudeTestServices.cs
+++ zeali-aptitude-test-core/ZealiAptitudeTestServices.cs
@@ -21,7 +21,7 @@ namespace zeali_aptitude_test_core
             List< AptitudeQuestions > allAptitudeQuestions = _aptitudeQuestions.Find(questions => true).ToList().OrderBy(i => Guid.NewGuid()).ToList();
             List<AptitudeQuestions> currentTestQuestions = new List<AptitudeQuestions>();
             int count = 0;
-            (AptitudeQuestions app in allAptitudeQuestions)
+            foreach (AptitudeQuestions app in allAptitudeQuestions)
             {
                 count = count + 1;
                 if (count <= 20)
