f86887b3d7aa107b476c23b26a3d761dd5bd871d
KarthickBoopathy <Karthickboopathy94@gmail.com>
2021-09-03 13:51:38 +0530

    remove warnings

--- zeali-aptitude-test/ClientApp/src/pages/Home/UserSettings.tsx
+++ zeali-aptitude-test/ClientApp/src/pages/Home/UserSettings.tsx
@@ -1,4 +1,4 @@
-import { Button, Paper } from "@material-ui/core";
+import { Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
